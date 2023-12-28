4e33c5c52138e49c5ab16e02aec5a7408428f772
jardimdanificado <jardeltorreslima01@gmail.com>
2023-12-28 12:15:35 -0300

    lua calls are now working

--- init.lua
+++ init.lua
@@ -5,13 +5,16 @@ local function example()
     local args = { "./src/entrypoint.pixi" }
 
     local child = luapiper.PipeSession(program, args)
-    child:send("clear #WHITE;print_int $test.a.b;frame 0;")
-    while true do
-        --local command = io.read()
-
-        child:send("clear #WHITE;frame 0;")
-    end
-
+    --os.execute("sleep 2")
+    child:send("system_new system;");
+    --child:send("print_int $system.config.height;");
+    child:send("load image0 ../data/img/0.jpg;");
+    child:send("layer_new $system $image0;");
+    --child:send("system ls;")
+    --child:send("list_push $system.layers $layer0;");
+    child:send("system_loop $system 0;")
+    print("image0 loaded\nfsafas\n")
+    os.execute("sleep 2") 
     child:close()
 end
 
