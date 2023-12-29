5d51c2552da1e88f991c0fb0cdad674a97b1b55b
jardimdanificado <jardeltorreslima01@gmail.com>
2023-12-29 09:55:36 -0300

    some advancement in lua integration

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local luapiper = require("lib.luapiper")
+local bruter = require("bruter")
 
 local function example()
     local program = "pixilang"
@@ -10,8 +11,6 @@ local function example()
     --child:send("print_int $system.config.height;");
     child:send("load image0 ../data/img/0.jpg;");
     child:send("layer_new $system $image0;");
-    --child:send("system ls;")
-    --child:send("list_push $system.layers $layer0;");
     child:send("system_loop $system 0;")
     print("image0 loaded\nfsafas\n")
     os.execute("sleep 2") 
