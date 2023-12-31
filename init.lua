83aaf5456f7de825a243e356108b207ab9aa15a6
jardimdanificado <jardeltorreslima01@gmail.com>
2023-12-31 17:47:35 -0300

    cleanup + new system

--- init.lua
+++ init.lua
@@ -1,23 +1,14 @@
-local luapiper = require("lib.luapiper")
 local bruter = require("bruter")
 
 local function example()
-    local program = "pixilang"
-    local args = { "./src/entrypoint.pixi" }
-
-    local child = luapiper.PipeSession(program, args)
-    --os.execute("sleep 2")
-    --bruter.child:send("system_new system;");
-    --bruter.system_new(child, "system", "data/config.ini");
-    --child:send("print_int $system.config.height;");
-    bruter.load("image0", "../data/img/0.jpg");
-    bruter.layer_new("image0");
-    bruter.system_loop();
-    --bruter.child:send("system_loop $system 0;");
-    print("image0 loaded\nfsafas\n");
+    local session = bruter.create()
+    local _ = session.run;
+    _ "system_new system;";
+    _ "load image0 ../data/img/0.jpg;";
+    _ "layer_new $system $image0;";
+    _ "system_loop $system 0;"
+    _ "close 0;"
     os.execute("sleep 2");
-    bruter.close();
-    
 end
 
 example()
