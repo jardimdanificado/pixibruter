f9808f5cd14859238c3b30f8501c00efe9ece004
jardimdanificado <jardeltorreslima01@gmail.com>
2023-12-31 17:19:09 -0300

    experimental before cleanup

--- init.lua
+++ init.lua
@@ -7,14 +7,17 @@ local function example()
 
     local child = luapiper.PipeSession(program, args)
     --os.execute("sleep 2")
-    child:send("system_new system;");
+    --bruter.child:send("system_new system;");
+    --bruter.system_new(child, "system", "data/config.ini");
     --child:send("print_int $system.config.height;");
-    child:send("load image0 ../data/img/0.jpg;");
-    child:send("layer_new $system $image0;");
-    child:send("system_loop $system 0;")
-    print("image0 loaded\nfsafas\n")
-    os.execute("sleep 2") 
-    child:close()
+    bruter.load("image0", "../data/img/0.jpg");
+    bruter.layer_new("image0");
+    bruter.system_loop();
+    --bruter.child:send("system_loop $system 0;");
+    print("image0 loaded\nfsafas\n");
+    os.execute("sleep 2");
+    bruter.close();
+    
 end
 
 example()
