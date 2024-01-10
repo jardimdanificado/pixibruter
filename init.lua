693538ba9aaca0a4e366354f61ad0579e9714742
jardimdanificado <jardeltorreslima01@gmail.com>
2024-01-10 09:44:39 -0300

    recursive should access indexes now

--- init.lua
+++ init.lua
@@ -17,7 +17,14 @@ local function example()
     local _ = session.run;
     _ "system_new:system;";
     _ "load:image0 ../data/img/0.jpg;";
+    _ "load:image1 ../data/img/1.jpg;";
+    _ "list_push $system.layers $image1;";
     _ "list_push $system.layers $image0;";
+    
+    _ "vector2:system.layers.3.position 150 190;";
+    _ "keyboard_add $system.keyboard #KEY_F2 0 @print_container_count;";
+    --$system.layers[2].position = vector2(190,150);
+    
     while true do
         _ "layers_render $system.layers;";
         _ "eventor $system;";
